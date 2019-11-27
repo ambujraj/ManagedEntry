38946e47d95aa62dc96e42687c762110e32c2889
Ambuj <thecoderguy@outlook.com>
2019-11-27 23:59:43 +0530

    ui and improved backend

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/managedEntry", {useNewUrlParser: true},{useUnifiedTopology: true});
+var entrySchema = new mongoose.Schema({
+	name: String,
+	email: String,
+   number: String,
+   checkint: String,
+   checkind: String,
+});
+var Entry = mongoose.model("Entry", entrySchema);
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.get("/", function(req, res){
    res.render("landing");
+});
+
+app.get("/checkin", function(req, res){
+var dt = new Date();
+var nam = req.query.name;
+var ema = req.query.email;
+var num = req.query.phone; 
+var time = dt.toTimeString();
+var date = dt.toDateString();
+Entry.create({name: nam, email: ema, number: num, checkint: time, checkind: date});
 });
 
 app.get("*", function(req, res){
